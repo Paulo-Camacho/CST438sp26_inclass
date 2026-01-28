a9ab3131fe5f0377728c5cfb373821d357e341a2
ciaranmoynihan <ciaran.moynihan@mac.com>
2026-01-27 19:04:23 -0800

    Added the two dropdowns, one for sorting, one for filtering.

--- app/src/main/java/com/example/mykotlinapplication/FreeToGameApi.kt
+++ app/src/main/java/com/example/mykotlinapplication/FreeToGameApi.kt
@@ -1,8 +1,11 @@
 package com.example.mykotlinapplication
-
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FreeToGameApi {
     @GET("api/games")
-    suspend fun getGames(): List<Game>
+    suspend fun getGames(
+        @Query("sort-by") sortBy: String? = null,
+        @Query("category") category: String? = null
+    ): List<Game>
 }
